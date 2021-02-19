f0ab86cafe0be21ab111179ec54b6f304471b4ef
GenryZ <genrymorganz@gmail.com>
2021-02-19 21:48:03 +0300

    add scrpt

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/reloader'
 
-get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+
+get '/' do 
+	erb :index
 end
