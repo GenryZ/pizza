d59d28a15c65de1ffcdff8d782e4bc3441a492dd
GenryZ <77881488+GenryZ@users.noreply.github.com>
2021-02-20 16:32:01 +0300

    Keep x variable in localStorage

--- public/app.js
+++ public/app.js
@@ -1,8 +1,10 @@
 function something()
 {
-	var x = window.localStorage.getItem('aaa');
+	var x = window.localStorage.getItem('bbb'); // получаем значение из localStorage
 
-	window.localStorage.setItem('aaa', 555);
-	
+	x = x * 1 + 1;								// увеличиваем это значение на 1
+
+	window.localStorage.setItem('bbb', x);		// устанавливаем значение ключа
+
 	alert(x);
 }
