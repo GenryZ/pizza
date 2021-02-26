3dd0cd2fb1cae2c5e4dc9d5d8a5ddb8b474d4a07
GenryZ <77881488+GenryZ@users.noreply.github.com>
2021-02-26 22:44:55 +0300

    replace id with product

--- app.rb
+++ app.rb
@@ -19,9 +19,14 @@ get '/about' do
 end
 post '/cart' do 
 	orders_input = params[:orders]
-	@orders = parse_orders_input orders_input
-	erb "Hello #{@orders.inspect}"
+	@items = parse_orders_input orders_input
+	@items.each do |item|
+		# id, cnt
+		item[0] = Product.find(item[0])
+	end
+	erb :cart
 end
+
 def parse_orders_input orders_input
 	s1 = orders_input.split(/,/)
 	arr = []	
